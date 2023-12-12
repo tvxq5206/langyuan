4aa7b2f3976360440c9fb97c569259e8b903dd00
tvxq5206 <tvxq5206@gmail.com>
2023-12-12 20:31:31 +0800

    修改遮罩&改sticky menu code

--- all.js
+++ all.js
@@ -2,16 +2,16 @@
 window.onscroll = function() {myFunction()};
 
 // Get the header
-const navbar = document.querySelector(".navbar");
+var navbar = document.getElementById("navbar-fixed");
 
 // Get the offset position of the navbar
-const sticky = navbar.offsetTop;
+var sticky = navbar.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
+    navbar.classList.add("sticky");
   } else {
-    header.classList.remove("sticky");
+    navbar.classList.remove("sticky");
   }
 }
