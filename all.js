b9cd76cd0a95a2eb9912a94c5c55148df3bded58
tvxq5206 <tvxq5206@gmail.com>
2023-12-16 08:40:52 +0800

    修改accordion變數

--- all.js
+++ all.js
@@ -19,39 +19,74 @@ function myFunction() {
 //了解更多按鈕hover
 const knowMoreBtn = document.querySelector(".know-more-btn");
 
-knowMoreBtn.addEventListener("mouseover", function(e){
+knowMoreBtn.addEventListener("mouseover", function (e) {
   e.target.setAttribute("src", "material/icon/arrow-right-circle-hover.svg");
 });
 
-knowMoreBtn.addEventListener("mouseout", function (e){
+knowMoreBtn.addEventListener("mouseout", function (e) {
   e.target.setAttribute("src", "material/icon/arrow-right-circle-default.svg");
 });
 
 //網頁初始化
-function init(){
+function init() {
   getLangInfo();
 };
 
-//串農業部領養平台API
-const langBreed = document.querySelector("#js-langBreed");
+init();
 
-let langData;
+//串農業部領養平台API
+const langInfo = document.querySelector(".js-langInfo");
+
+let langData = [];
 let str = "";
-function getLangInfo(){
-  axios.get(`https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=50&$skip=0`)
-  .then(function(response){
-    langData = response.data[0].animal_Variety;
-    renderLangList();
-  })
+
+function getLangInfo() {
+  axios.get(`https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=10&$skip=0`)
+    .then(function (response) {
+      langData = response.data;
+      renderLangList();
+    })
 };
+
 
 //渲染全部的條件
 function renderLangList() {
   //已被填入陣列的資料，可以用forEach遍歷其中的資料
   langData.forEach(function (item) {
     //利用上方已宣告的空字串組字串
-    str += `<p class="text-secondary1" id="js-langBreed">${item[0].animal_Variety}</p>`
+    str += `<div class="col">
+    <div class="text-end name-breed-position">
+        <p class="text-secondary1">${item.animal_Variety}</p>
+        <p class="h5-font-size">${item.animal_colour}</p>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="doggo-left-mask lang-index-container img-fluid"></div>
+        </div>
+        <div class="row justify-content-center mt-4">
+            <div class="w-100"></div>
+            <div class="col-2">
+                <p class="text-secondary1 text-center">類別</p>
+                <img src="material/icon/dog.svg" alt="">
+            </div>
+            <div class="col-2 text-borderline">
+                <p class="text-secondary1 text-center mb-2">年齡</p>
+                <p class="text-center" style="font-size: 16px">${item.animal_age}</p>
+            </div>
+            <div class="col-2">
+                <p class="text-secondary1 text-center">性別</p>
+                <img src="material/icon/male.svg" alt="">
+            </div>
+            <div class="w-100"></div>
+            <div class="text-center col-8 mt-3">
+                <p>
+                    <img src="material/icon/map-marker-alt.svg" alt="">${item.animal_place}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>`;
   })
   //利用上方已宣告的productList(抓到的DOM)填入剛組好的字串(html標籤)
-  langBreed.innerHTML = str;
+  langInfo.innerHTML = str;
 };
--- toggle_switch.js
+++ toggle_switch.js
@@ -1,10 +1,23 @@
 const toggleButton = document.querySelector(".toggle-switch");
+const dogInfo = document.getElementById("dogInfo");
+const catInfo = document.getElementById("catInfo");
 
 const togglePet = () => {
-	let pet = toggleButton.getAttribute("data-pet");
-	toggleButton.setAttribute("aria-label", `Switch to ${pet}`);
-	pet = pet === "dog" ? "cat" : "dog";
-	toggleButton.setAttribute("data-pet", pet);
+    let pet = toggleButton.getAttribute("data-pet");
+    toggleButton.setAttribute("aria-label", `Switch to ${pet}`);
+
+    // 切換顯示的區塊
+    if (pet === "dog") {
+        dogInfo.style.display = "block";
+        catInfo.style.display = "none";
+    } else {
+        dogInfo.style.display = "none";
+        catInfo.style.display = "block";
+    }
+
+    // 切換寵物
+    pet = pet === "dog" ? "cat" : "dog";
+    toggleButton.setAttribute("data-pet", pet);
 };
 
 toggleButton.addEventListener("click", togglePet);
